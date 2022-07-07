105836f3da4afd282d04629115494699a073c6a4
Sunilp654 <sunilkumarp654@gmail.com>
2022-07-07 20:51:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-ejqjwz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ejqjwz)
---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import Nav from './Comp/Nav';
+import CarouselS from './Comp/CarouselS';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <CarouselS />
+    </>
+  );
+}
+
+export default App;
